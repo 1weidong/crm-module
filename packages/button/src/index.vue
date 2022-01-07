aaca31944c06e084416d2eafd27c9be409c7e616
zhangweidong <zhangweidong@uhomes.com>
2022-01-07 08:37:41 +0800

    添加Button按钮

---
+++ packages/button/src/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div @click="clickMe">
+        点我
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-property-decorator';
+export default class Botton extends Vue {
+    clickMe() {
+        alert(1111)
+    }
+}
+</script>
